3ab5c26afef604dcdeb9f02a8dff958bba343265
Cezar Garrido <cezar.cgb18@outlook.com>
2020-03-23 09:34:04 -0300

    chore: Adcionado header bearer

--- services/digesto/digesto.go
+++ services/digesto/digesto.go
@@ -25,7 +25,10 @@ func (this *Digesto) CreateUserCompany(ent *UserCompany) (userCompany *UserCompa
 	if err != nil {
 		return
 	}
-	response, err := http.DefaultClient.Do(req)
+	req.Header.Set("Authorization", "Bearer 708c3227-156a-4dba-87e4-4a0061a671d2")
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := http.client.Do(req)
 	if err != nil {
 		return
 	}
