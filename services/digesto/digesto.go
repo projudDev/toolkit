4e06ec4a78b785de74db86b114c5ae1348169c97
Cezar Garrido <cezar.cgb18@outlook.com>
2020-03-24 17:49:47 -0300

    chore:tratamentos de erros e crud da parte

--- services/digesto/digesto.go
+++ services/digesto/digesto.go
@@ -5,10 +5,9 @@ import "bytes"
 import "net/http"
 import "errors"
 import "strconv"
-import "io/ioutil"
 
 const (
-	host = "https://op.digesto.com.br/api/"
+	host = "https://op.digesto.com.br/api"
 )
 
 type Digesto struct {
@@ -60,18 +59,18 @@ func (this *Digesto) DeleteUserCompany(id int64) error {
 	}
 	req.Header.Set("Authorization", "Bearer "+this.ApiKey)
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	response, err := client.Do(req)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
-	query, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
+	defer response.Body.Close()
 
-	if resp.StatusCode != 204 {
-		return errors.New(string(query))
+	if response.StatusCode != 204 {
+		var e Erro
+		if err = json.NewDecoder(response.Body).Decode(&e); err != nil {
+			return errors.New("Error: Fail converting digesto error" + err.Error())
+		}
+		return errors.New("Message: " + e.Message + " Status: " + strconv.Itoa(e.Status))
 	}
 
 	return nil
@@ -85,14 +84,18 @@ func (this *Digesto) RestoreUserCompany(id int64) error {
 	}
 	req.Header.Set("Authorization", "Bearer "+this.ApiKey)
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	response, err := client.Do(req)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return errors.New("Não foi possivel restaurar a empresa")
+	if response.StatusCode != 200 {
+		var e Erro
+		if err = json.NewDecoder(response.Body).Decode(&e); err != nil {
+			return errors.New("Error: Fail converting digesto error" + err.Error())
+		}
+		return errors.New("Message: " + e.Message + " Status: " + strconv.Itoa(e.Status))
 	}
 	return nil
 }
@@ -181,4 +184,84 @@ func (this *Digesto) AdminApiToken(digestoID string) (userCompany *UserCompany, 
 	}
 	err = json.NewDecoder(response.Body).Decode(&userCompany)
 	return
+}
+
+func (this *Digesto) UpdateMonitoredPerson(id int64, ent *MonitoredPerson) (monitoredPerson *MonitoredPerson, err error) {
+
+	idS := strconv.FormatInt(id, 10)
+	body, err := json.Marshal(ent)
+	if err != nil {
+		return
+	}
+	req, err := http.NewRequest("PATCH", this.Host+"/monitored_person/"+idS, bytes.NewReader(body))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+this.ApiKey)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	if response.Status != "200 OK" {
+		var e Erro
+		if err = json.NewDecoder(response.Body).Decode(&e); err != nil {
+			err = errors.New("Error: Fail converting digesto error" + err.Error())
+			return
+		}
+		err = errors.New("Message: " + e.Message + " Status: " + strconv.Itoa(e.Status))
+		return
+	}
+	err = json.NewDecoder(response.Body).Decode(&monitoredPerson)
+	return
+}
+
+func (this *Digesto) DeleteMonitoredPerson(id int64) error {
+	idS := strconv.FormatInt(id, 10)
+	req, err := http.NewRequest("DELETE", this.Host+"/monitored_person/"+idS, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+this.ApiKey)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 204 {
+		var e Erro
+		if err = json.NewDecoder(response.Body).Decode(&e); err != nil {
+			return errors.New("Error: Fail converting digesto error" + err.Error())
+		}
+		return errors.New("Message: " + e.Message + " Status: " + strconv.Itoa(e.Status))
+	}
+
+	return nil
+}
+
+func (this *Digesto) RestoreMonitoredPerson(id int64) error {
+	idS := strconv.FormatInt(id, 10)
+	req, err := http.NewRequest("POST", this.Host+"/monitored_person/archive/"+idS+"/restore", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+this.ApiKey)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		var e Erro
+		if err = json.NewDecoder(response.Body).Decode(&e); err != nil {
+			return errors.New("Error: Fail converting digesto error" + err.Error())
+		}
+		return errors.New("Message: " + e.Message + " Status: " + strconv.Itoa(e.Status))
+	}
+	return nil
 }
